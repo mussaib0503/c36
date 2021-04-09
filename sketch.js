72bb2e872974897a17a58f087e060b573e0e4b2b
mussaib0503 <77156422+mussaib0503@users.noreply.github.com>
2021-04-09 16:02:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canvas,backgroundImage;
+var gameState=0;
+var playerCount;
+var database;
+var game;
+var player;
+var form;
+function setup(){
+canvas=createCanvas(400,400);
+database=firebase.database;
+}
+function draw(){
+  
+}
+
